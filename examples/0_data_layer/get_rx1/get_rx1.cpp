82fe7ef141afefbe4f2d569ec51f477cc4a24203
bitconnector <luk-krat@web.de>
2021-12-10 23:16:57 +0100

    added reciving rx1 downlink

---
+++ examples/0_data_layer/get_rx1/get_rx1.cpp
@@ -0,0 +1,124 @@
+#include <Arduino.h>
+
+#include <SPI.h>
+#include "LoRa.h"
+
+// LoRa Pins
+#define LoRa_RST 23
+#define LoRa_CS 18
+#define LoRa_DIO0 26
+#define LoRa_DIO1 33
+#define LoRa_DIO2 32
+#define LoRa_SPI SPI
+
+#include "LoraWANmessage.hpp"
+LoraWANmessage message = LoraWANmessage();
+
+void setup()
+{
+    Serial.begin(115200);
+    delay(500);
+    Serial.println(F("Starting"));
+    esp_random();
+
+    message.setDevAddr("260BCCD8");
+    message.setNwkSKey("8D53AF486F7B992BE91A8193242554D7");
+    message.setAppSKey("8CE6E6034DBAF7A01F7EAC95386B0C09");
+
+    LoRa.setPins(LoRa_CS, LoRa_RST, LoRa_DIO0);
+}
+
+void printPackage(char *data, uint16_t size, bool structure);
+void onReceive(int packetSize);
+
+void loop()
+{
+    char payload[10];
+    sprintf(payload, "r=%02x", random(256));
+    Serial.printf("Sending: %s\n", payload);
+
+    if (!LoRa.begin(868100000))
+    {
+        Serial.println("LoRa init failed. Check your connections.");
+        while (true)
+            ; // if failed, do nothing
+    }
+    LoRa.setPreambleLength(8);
+    LoRa.setSyncWord(0x34);
+    LoRa.enableCrc();
+    LoRa.disableInvertIQ();
+    LoRa.setCodingRate4(5);
+    LoRa.setSpreadingFactor(7);
+    LoRa.setSignalBandwidth(125E3);
+
+    message.uplink(payload, strlen(payload), 5);
+    printPackage((char *)message.data, message.dataLen, 1);
+
+    LoRa.beginPacket(); // start packet
+    LoRa.write(message.data, message.dataLen);
+    LoRa.endPacket(); // finish packet and send it
+    unsigned long txTime = millis();
+
+    //RX1
+    LoRa.disableCrc();
+    LoRa.enableInvertIQ();
+    while (txTime + 10000 > millis())
+    {
+        onReceive(LoRa.parsePacket());
+    }
+    Serial.println("end RX1");
+
+    LoRa.sleep();
+    sleep(60);
+}
+
+void onReceive(int packetSize)
+{
+    if (packetSize == 0)
+        return; // if there's no packet, return
+
+    // received a packet
+    Serial.print("Received packet\n");
+
+    // read packet
+    char buf[200];
+    int bufLen = 0;
+    while (LoRa.available())
+    {
+        buf[bufLen] = LoRa.read();
+        bufLen++;
+    }
+    printPackage(buf, bufLen, 1);
+
+    // print RSSI of packet
+    Serial.print("with RSSI ");
+    Serial.println(LoRa.packetRssi());
+}
+
+void printPackage(char *data, uint16_t size, bool structure)
+{
+    Serial.print("+ Package:");
+    for (int i = 0; i < size; i++)
+        Serial.printf(" %02x", data[i]);
+    if (structure)
+    {
+        Serial.print("\n          |hd|   addr    |Ct| FCnt|");
+        if (size < 13)
+        {
+            Serial.println("    MIC    |");
+        }
+        else if (size == 13)
+        {
+            Serial.println("Pt|    MIC    |");
+        }
+        else
+        {
+            Serial.print("Pt|");
+            for (int i = 14; i < size; i++)
+                Serial.print("   ");
+            Serial.println("  |    MIC    |");
+        }
+    }
+    else
+        Serial.println();
+}
